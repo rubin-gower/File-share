83fe6d34b8e425dc47f37fbf4d4f9ba52fc8b81f
rubin-gower <rubinsemails@gmail.com>
2020-08-21 13:43:38 +1200

    cors middleweare

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const express = require('express')
+const multer = require('multer')
+const cors = require('cors');
 
 const download = require("./routes/downloads")
 const files = require("./routes/files")
 
 const server = express()
 
+server.use(cors())
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
